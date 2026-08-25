88ddfb491e3d0ccb47973f40f3a88da2706b3150
Amisha Chaubey <amishaaryani1@gmail.com>
2026-08-25 10:40:17 +0530

    update

--- frontend/src/components/notifications/NotificationList.jsx
+++ frontend/src/components/notifications/NotificationList.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNotifications } from './useNotifications';
-import NotificationItem from './NotificationItem';
+import NotificationItem from './NotificationItems';
 import NotificationDetailModal from './NotificationDetailModal';
 
 export default function NotificationList() {
